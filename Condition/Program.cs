90573c28ebcafbd9717a43111b34608a511e2175
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-06-14 20:35:07 +0600

    file add complete

---
+++ Condition/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Condition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if(n == 0 || n == 1)
+            {
+                Console.WriteLine("0 or 1 is not a prime number.");
+            }
+            else
+            {
+                Console.WriteLine("Prime numbers are :");
+                for (int i=2; i<=n; i++)
+                {
+                   
+
+                    if (i == 2)
+                        Console.WriteLine(i);
+                    if (i % 2 != 0)
+                        Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
